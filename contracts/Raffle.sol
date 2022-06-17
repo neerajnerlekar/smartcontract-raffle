d45098d46199df1e3331004f6ae738bbcd351a8c
neerajnerlekar <neeraj.nerlekar@gmail.com>
2022-06-17 10:27:01 -0600

    complete function checkUpkeep

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -10,11 +10,19 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
 error Raffle__NotEnoughEntryFee();
 error Raffle__TransferFailed();
+error Raffle__NotOpen();
 
-contract Raffle is VRFConsumerBaseV2 {
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    /* Type Variables */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     /* State Variables */
 
     // using i_entranceFee to know that it's an immutable variable which will use less gas
@@ -34,6 +42,10 @@ contract Raffle is VRFConsumerBaseV2 {
 
     /* Lottery Variables */
     address private s_recentWinner;
+    // creating a variable of type RaffleState and initializing it in the construstor to "open" state
+    RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
     /* Events */
     event RaffleEnter(address indexed player);
@@ -48,19 +60,27 @@ contract Raffle is VRFConsumerBaseV2 {
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     function enterRaffle() public payable {
         // gas costly way -> require(msg.value > i_entranceFee, "Not enough ETH!")
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEntryFee();
+        }
+        // checking the condition 4 below to ensure the player can only enter in an "open" state
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
         }
         s_players.push(payable(msg.sender));
         // emit an event when we update a dynamic array or mapping
@@ -73,7 +93,36 @@ contract Raffle is VRFConsumerBaseV2 {
     // So, splitting the function pickRandomWinner -> requestRandomWinner and fulfillRandomWords
     // function pickRandomWinner() external {}
 
+    /**
+     * @dev This is the function that Chainlink KEeper nodes call
+     * they look for the `upkeepNeeded` to return true
+     * The following conditions should be met for the upkeep to return true
+     * 1. our time interval should have passed
+     * 2. the lottery should have at least 1 player, and have ETH in the contract
+     * 3. our subscription is funded with LINK
+     * 4. the lottery should be in an "open" state
+     */
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+    }
+
     function requestRandomWinner() external {
+        // setting the state to calculating, so no players can enter in this time period
+        s_raffleState = RaffleState.CALCULATING;
         // i_vrfCoordinator is our contract address variable for requestRandomness contract
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane, // for us to use this keyHash -> storing it as a state variable gasLane and passing it in the constructor
@@ -93,6 +142,9 @@ contract Raffle is VRFConsumerBaseV2 {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        // resetting the players array
+        s_players = new address payable[](0);
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
